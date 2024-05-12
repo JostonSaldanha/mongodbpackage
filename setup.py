2a4dbc18405f2c4d78361744bf1b162451be14e6
JostonSaldanha <jostonsaldanha0@gmail.com>
2024-05-13 02:12:31 +0530

    commit

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 __version__ = "0.0.1"
 REPO_NAME = "mongodbpackage"
 PKG_NAME= "Mongodb_JConnect"
-AUTHOR_USER_NAME = "Joston_Saldanha"
+AUTHOR_USER_NAME = "JostonSaldanha"
 AUTHOR_EMAIL = "jostonsaldanha0@gmail.com"
 
 setup(
